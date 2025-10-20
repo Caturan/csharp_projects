30aa6bce2ec3b08d6f34de683fe05651139bc6cf
Caturan <cevdetahmet.turan@enmdigital.com>
2025-10-20 22:19:08 +0300

    commi

---
+++ C#Kurs/Project_DapperNorthwind/Repositories/CategoryRepositories/CategoryRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project_DapperNorthwind.Dtos.CategoryDtos;
+
+namespace Project_DapperNorthwind.Repositories.CategoryRepositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        Task ICategoryRepository.CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task ICategoryRepository.DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<List<ResultCategoryDto>> ICategoryRepository.GetAllCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<GetByIdCategoryDto> ICategoryRepository.GetCategoryByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task ICategoryRepository.UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ C#Kurs/Project_DapperNorthwind/Repositories/CategoryRepositories/ICategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project_DapperNorthwind.Dtos.CategoryDtos;
+
+namespace Project_DapperNorthwind.Repositories.CategoryRepositories
+{
+    public interface ICategoryRepository
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoriesAsync();    
+
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);  
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(int id); 
+        Task<GetByIdCategoryDto> GetCategoryByIdAsync(int id);  
+
+    }
+}
